ba3ad77f2501fdb94f6e0b2e40c234f3e286ea84
Santhosh Raju <msr@Santhoshs-MacBook-Air.local>
2026-08-24 00:11:43 +0530

    Prisma Client

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -6,7 +6,7 @@ export default async function Page(){
   const session=await getServerSession(authOptions);
 
   if(session?.user){
-    redirect('/p2p_Transfer');
+    redirect('/transfer');
   }
   else {
     redirect('/api/auth/signin');
